a7697f8b6e72ff54b05e387a57f2855c64dc8ef2
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-05-11 23:56:54 +0300

    added missing files (from last commit)

---
+++ Voron.Graph/Primitives/Edge.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voron.Graph
+{
+    public class Edge : IDisposable
+    {
+        public string KeyFrom { get; set; }
+
+        public string KeyTo { get; set; }
+
+        public Stream Data { get; private set; }
+
+        public Edge(string keyFrom, string keyTo, Stream data = null, bool makeDataCopy = true)
+        {
+            KeyFrom = keyFrom;
+            KeyTo = keyTo;
+            if (data == null)
+                Data = Stream.Null;
+            else if (makeDataCopy)
+            {
+                Data = new BufferedStream(new MemoryStream());
+                data.CopyTo(Data);
+                Data.Position = 0;
+            }
+            else
+                Data = data;
+        }
+
+        public Edge(string keyFrom, string keyTo, byte[] data)
+        {
+            KeyFrom = keyFrom;
+            KeyTo = keyTo;
+
+            Data = (data != null) ? new MemoryStream(data) : Stream.Null;
+        }
+
+        public void Dispose()
+        {
+            if (Data != null)
+            {
+                Data.Dispose();
+                Data = null;
+            }
+                
+        }
+    }
+}
---
+++ Voron.Graph/Primitives/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voron.Graph
+{
+    public class Node : IDisposable
+    {
+        public string Key { get; set; }
+
+        public Stream Data { get; private set; }
+
+        public Node(string key, Stream data, bool makeDataCopy = true)
+        {
+            Key = key;
+            if(makeDataCopy == true)
+            {
+                Data = new MemoryStream();
+                data.CopyTo(Data);
+                Data.Position = 0;
+            }
+            else
+                Data = data;
+        }
+
+        public Node(string key, byte[] data)
+        {
+            Key = key;
+            Data = (data != null) ? new MemoryStream(data) : Stream.Null;
+        }
+
+        public void Dispose()
+        {
+            if (Data != null)
+            {
+                Data.Dispose();
+                Data = null;
+            }
+                
+        }
+    }
+}
