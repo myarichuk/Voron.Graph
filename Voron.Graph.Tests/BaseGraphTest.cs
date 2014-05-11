39977d9bf0dd8c251c543a2109ee65b9765cfe7c
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-05-12 01:46:31 +0300

    added some safety precautions

--- Voron.Graph.Tests/BaseGraphTest.cs
+++ Voron.Graph.Tests/BaseGraphTest.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Concurrent;
 
 namespace Voron.Graph.Tests
 {
@@ -11,22 +12,30 @@ namespace Voron.Graph.Tests
     public class BaseGraphTest
     {
         protected StorageEnvironment Env;
+        protected ConcurrentQueue<IDisposable> DisposalQueue;
 
         [TestInitialize]
         public void BeforeTest()
         {
             Env = new StorageEnvironment(StorageEnvironmentOptions.CreateMemoryOnly());
+            DisposalQueue = new ConcurrentQueue<IDisposable>();
         }
 
         [TestCleanup]
         public void AfterTest()
         {
             Env.Dispose();
+            foreach (var disposable in DisposalQueue.Where(x => x != null))
+                disposable.Dispose();
         }
 
         public Stream StreamFrom(string s)
         {
-            return new MemoryStream(Encoding.UTF8.GetBytes(s));
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(s));
+            //just to be sure
+            DisposalQueue.Enqueue(stream);
+
+            return stream;
         }
 
         public string StringFrom(Stream stream)
@@ -36,6 +45,7 @@ namespace Voron.Graph.Tests
             while (stream.Read(buffer, 0, 128) > 0)
                 readBytes.AddRange(buffer.Where(x => x != 0));
 
+            DisposalQueue.Enqueue(stream);
             return Encoding.UTF8.GetString(readBytes.ToArray());
         }
     }
