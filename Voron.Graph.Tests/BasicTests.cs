79a41fd253fdf95be34b393a49166eeb385dfe1e
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-05-14 02:10:51 +0300

    refactoring/reimplementation WIP. uncommented relevant unit tests

--- Voron.Graph.Tests/BasicTests.cs
+++ Voron.Graph.Tests/BasicTests.cs
@@ -9,48 +9,107 @@ namespace Voron.Graph.Tests
     [TestClass]
     public class BasicTests : BaseGraphTest
     {
-        //[TestMethod]
-        //public void Put_nodes_should_work()
-        //{
-        //    var graph = new GraphEnvironment("TestGraph", Env);
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        session.PutNode("n1", StreamFrom("test1"));
-        //        session.PutNode("n2", StreamFrom("test2"));
-        //        session.PutNode("n3", StreamFrom("test3"));
-        //        session.SaveChanges();
-        //    }
+        [TestMethod]
+        public void Put_nodes_should_work()
+        {
+            var graph = new GraphEnvironment("TestGraph", Env);
+            using (var session = graph.OpenSession())
+            {
+                session.PutNode("n1", StreamFrom("test1"));
+                session.PutNode("n2", StreamFrom("test2"));
+                session.PutNode("n3", StreamFrom("test3"));
+                session.SaveChanges();
+            }
 
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        StringFrom(session.GetNode("n1")).ShouldBeEquivalentTo("test1");
-        //        StringFrom(session.GetNode("n2")).ShouldBeEquivalentTo("test2");
-        //        StringFrom(session.GetNode("n3")).ShouldBeEquivalentTo("test3");
-        //    }
-        //}
+            using (var session = graph.OpenSession())
+            {
+                StringFrom(session.GetNode("n1")).ShouldBeEquivalentTo("test1");
+                StringFrom(session.GetNode("n2")).ShouldBeEquivalentTo("test2");
+                StringFrom(session.GetNode("n3")).ShouldBeEquivalentTo("test3");
+            }
+        }
 
-        //[TestMethod]
-        //public void Put_edge_between_nodes_in_the_same_session_should_work()
-        //{
-        //    var graph = new GraphEnvironment("TestGraph", Env);
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        session.PutNode("n1", StreamFrom("test1"));
-        //        session.PutNode("n2", StreamFrom("test2"));
-        //        session.PutNode("n3", StreamFrom("test3"));
+        [TestMethod]
+        public void Put_edge_between_nodes_in_the_same_session_should_work()
+        {
+            var graph = new GraphEnvironment("TestGraph", Env);
+            using (var session = graph.OpenSession())
+            {
+                session.PutNode("n1", StreamFrom("test1"));
+                session.PutNode("n2", StreamFrom("test2"));
+                session.PutNode("n3", StreamFrom("test3"));
 
-        //        session.PutEdge("n2", "n1");
-        //        session.PutEdge("n2", "n3");
+                session.PutEdge("n2", "n1");
+                session.PutEdge("n2", "n3");
 
-        //        session.SaveChanges();
-        //    }
+                session.SaveChanges();
+            }
 
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        var adjacentNodes = session.GetAdjacent("n2");
-        //        adjacentNodes.Should().Contain("n1", "n3");
-        //    }
-        //}
+            using (var session = graph.OpenSession())
+            {
+                var adjacentNodes = session.GetAdjacent("n2");
+                adjacentNodes.Should().Contain("n1", "n3");
+            }
+        }
+
+        [TestMethod]
+        public void Can_get_edge_data()
+        {
+            var graph = new GraphEnvironment("TestGraph", Env);
+            using (var session = graph.OpenSession())
+            {
+                session.PutNode("n1", StreamFrom("test1"));
+                session.PutNode("n2", StreamFrom("test2"));
+                session.PutNode("n3", StreamFrom("test3"));
+
+                session.PutEdge("n2", "n1", StreamFrom("FooBar1"));
+                session.PutEdge("n2", "n3", StreamFrom("FooBar2"));
+                session.PutEdge("n1", "n3", StreamFrom("FooBar3"));
+
+                session.SaveChanges();
+            }
+
+            using (var session = graph.OpenSession())
+            {
+                StringFrom(session.GetEdge("n2", "n3")).Should().BeEquivalentTo("FooBar2");
+                StringFrom(session.GetEdge("n2", "n1")).Should().BeEquivalentTo("FooBar1");
+                StringFrom(session.GetEdge("n1", "n3")).Should().BeEquivalentTo("FooBar3");
+            }
+        }
+
+
+        [TestMethod]
+        public void Put_edge_between_nodes_in_different_session_should_work()
+        {
+            var graph = new GraphEnvironment("TestGraph", Env);
+            using (var session = graph.OpenSession())
+            {
+                session.PutNode("n1", StreamFrom("test1"));
+                session.PutNode("n2", StreamFrom("test2"));
+                session.PutNode("n3", StreamFrom("test3"));
+
+                session.SaveChanges();
+            }
+
+            using (var session = graph.OpenSession())
+            {
+                session.PutEdge("n2", "n1");
+                session.PutEdge("n2", "n3");
+
+                //looping edge also ok!
+                //adding multiple loops will overwrite each other
+                //TODO: add support for multiple edges that have the same keyFrom and keyTo
+                session.PutEdge("n2", "n2");
+
+                session.SaveChanges();
+            }
+
+            using (var session = graph.OpenSession())
+            {
+                var adjacentNodes = session.GetAdjacent("n2");
+                adjacentNodes.Should().Contain("n1", "n3", "n2");
+            }
+        }
 
         [TestMethod]
         public void Can_iterate_on_nodes()
@@ -119,63 +178,6 @@ namespace Voron.Graph.Tests
             }
         }
 
-        //[TestMethod]
-        //public void Can_get_edge_data()
-        //{
-        //    var graph = new GraphEnvironment("TestGraph", Env);
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        session.PutNode("n1", StreamFrom("test1"));
-        //        session.PutNode("n2", StreamFrom("test2"));
-        //        session.PutNode("n3", StreamFrom("test3"));
-
-        //        session.PutEdge("n2", "n1", StreamFrom("FooBar1"));
-        //        session.PutEdge("n2", "n3", StreamFrom("FooBar2"));
-        //        session.PutEdge("n1", "n3", StreamFrom("FooBar3"));
-
-        //        session.SaveChanges();
-        //    }
-
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        StringFrom(session.GetEdge("n2", "n3")).Should().BeEquivalentTo("FooBar2");
-        //        StringFrom(session.GetEdge("n2", "n1")).Should().BeEquivalentTo("FooBar1");
-        //        StringFrom(session.GetEdge("n1", "n3")).Should().BeEquivalentTo("FooBar3");
-        //    }
-        //}
-
-
-        //[TestMethod]
-        //public void Put_edge_between_nodes_in_different_session_should_work()
-        //{
-        //    var graph = new GraphEnvironment("TestGraph", Env);
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        session.PutNode("n1", StreamFrom("test1"));
-        //        session.PutNode("n2", StreamFrom("test2"));
-        //        session.PutNode("n3", StreamFrom("test3"));
-                
-        //        session.SaveChanges();
-        //    }
-
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        session.PutEdge("n2", "n1");
-        //        session.PutEdge("n2", "n3");
-
-        //        //looping edge also ok!
-        //        //adding multiple loops will overwrite each other
-        //        //TODO: add support for multiple edges that have the same keyFrom and keyTo
-        //        session.PutEdge("n2", "n2"); 
-
-        //        session.SaveChanges();
-        //    }
-
-        //    using (var session = graph.OpenSession())
-        //    {
-        //        var adjacentNodes = session.GetAdjacent("n2");
-        //        adjacentNodes.Should().Contain("n1", "n3", "n2");
-        //    }
-        //}
+      
     }
 }
