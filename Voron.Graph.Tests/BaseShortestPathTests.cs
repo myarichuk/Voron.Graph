97c0e5a365f0b05d5ba6a8844fd72f9bf7d23025
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-05-31 13:01:31 +0300

    refactored shortest path tests to share test code

---
+++ Voron.Graph.Tests/BaseShortestPathTests.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Voron.Graph.Algorithms.ShortestPath;
+using Voron.Graph.Extensions;
+using FluentAssertions;
+using Voron.Graph.Exceptions;
+using Voron.Graph.Algorithms.ShortestPath;
+
+namespace Voron.Graph.Tests
+{
+    [TestClass]
+    public abstract class BaseShortestPathTests : BaseGraphTest
+    {
+        protected CancellationTokenSource cancelTokenSource;
+
+        protected abstract IShortestPathAlgorithm GetAlgorithm(Transaction tx, GraphStorage graph, Node rootNode);
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            cancelTokenSource = new CancellationTokenSource();
+        }
+
+        /*
+         *   node1 ----> node3
+         *     |          ^ 
+         *     L-> node2 / 
+         */
+        [TestMethod]
+        public void Simple_shortest_path_with_constant_edge_weight_should_work()
+        {
+            var graph = new GraphStorage("TestGraph", Env);
+
+            Node node1, node2, node3;
+            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
+                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
+                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
+
+                node1.ConnectWith(tx, node2, graph);
+                node1.ConnectWith(tx, node3, graph);
+                node2.ConnectWith(tx, node3, graph);
+
+                tx.Commit();
+            }
+
+            using (var tx = graph.NewTransaction(TransactionFlags.Read))
+            {
+                var shortestPathsData = GetAlgorithm(tx,graph,node1).Execute();
+
+                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node3).ToList();
+                shortestNodePath.Should().ContainInOrder(node1.Key, node3.Key);
+            }
+        }
+
+
+
+        /*
+               *   node1 ----> node3
+               *     |          /|\
+               *     L-> node2 --|
+               * 
+               */
+        [TestMethod]
+        public void Simple_shortest_path_with_varying_edge_weight_should_work()
+        {
+            var graph = new GraphStorage("TestGraph", Env);
+
+            Node node1, node2, node3;
+            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
+                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
+                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
+
+                node1.ConnectWith(tx, node2, graph, 2);
+                node1.ConnectWith(tx, node3, graph, 10);
+                node2.ConnectWith(tx, node3, graph, 2);
+
+                tx.Commit();
+            }
+
+            using (var tx = graph.NewTransaction(TransactionFlags.Read))
+            {
+                var shortestPathsData = GetAlgorithm(tx, graph, node1).Execute();
+
+                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node3).ToList();
+                shortestNodePath.Should().ContainInOrder(node1.Key, node2.Key, node3.Key);
+            }
+        }
+
+
+        /*
+         *       ^ node2 
+         *      /        \
+         * node1          > node4
+         *      \> node3 /
+         * 
+         */
+        [TestMethod]
+        public void Between_two_equivalent_paths_first_created_should_be_chosen()
+        {
+            var graph = new GraphStorage("TestGraph", Env);
+
+            Node node1, node2, node3, node4;
+            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
+                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
+                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
+                node4 = graph.Commands.CreateNode(tx, JsonFromValue(4));
+
+                node1.ConnectWith(tx, node2, graph, 2);
+                node2.ConnectWith(tx, node4, graph, 1);
+
+                node1.ConnectWith(tx, node3, graph, 1);
+                node3.ConnectWith(tx, node4, graph, 2);
+
+                tx.Commit();
+            }
+
+            using (var tx = graph.NewTransaction(TransactionFlags.Read))
+            {
+                var shortestPathsData = GetAlgorithm(tx, graph, node1).Execute();
+
+                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node4).ToList();
+                shortestNodePath.Should().ContainInOrder(node1.Key, node2.Key, node4.Key);
+            }
+        }
+
+        /*       ^ node2 -> node3 
+         *      /                \
+         * node1 ---------------- > node4
+         */
+        [TestMethod]
+        public void Cheap_long_path_over_short_expensive_path_should_be_chosen()
+        {
+            var graph = new GraphStorage("TestGraph", Env);
+
+            Node node1, node2, node3, node4;
+            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
+                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
+                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
+                node4 = graph.Commands.CreateNode(tx, JsonFromValue(4));
+
+                node1.ConnectWith(tx, node2, graph, 1);
+                node2.ConnectWith(tx, node3, graph, 1);
+                node3.ConnectWith(tx, node4, graph, 1);
+
+                node1.ConnectWith(tx, node4, graph, 10);
+
+                tx.Commit();
+            }
+
+            using (var tx = graph.NewTransaction(TransactionFlags.Read))
+            {
+                var shortestPathsData = GetAlgorithm(tx, graph, node1).Execute();
+
+                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node4);
+                shortestNodePath.Should().ContainInOrder(node1.Key, node2.Key, node3.Key, node4.Key);
+            }
+        }
+    }
+}
--- Voron.Graph.Tests/DijkstraTests.cs
+++ Voron.Graph.Tests/DijkstraTests.cs
@@ -14,50 +14,8 @@ namespace Voron.Graph.Tests
 {
     //TODO: add more tests
     [TestClass]
-    public class DijkstraTests : BaseGraphTest
+    public class DijkstraTests : BaseShortestPathTests
     {
-        private CancellationTokenSource cancelTokenSource;
-
-        [TestInitialize]
-        public void InitTest()
-        {
-            cancelTokenSource = new CancellationTokenSource();
-        }
-
-        /*
-         *   node1 ----> node3
-         *     |          ^ 
-         *     L-> node2 / 
-         * 
-         */
-        [TestMethod]
-        public void Simple_shortest_path_with_constant_edge_weight_should_work()
-        {
-            var graph = new GraphStorage("TestGraph", Env);
-
-            Node node1, node2, node3;
-            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
-            {
-                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
-                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
-                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
-
-                node1.ConnectWith(tx, node2, graph);
-                node1.ConnectWith(tx, node3, graph);
-                node2.ConnectWith(tx, node3, graph);
-
-                tx.Commit();
-            }
-
-            using(var tx = graph.NewTransaction(TransactionFlags.Read))
-            {
-                var shortestPathAlgorithm = new DijkstraShortestPath(tx, graph, node1, cancelTokenSource.Token);
-                var shortestPathsData = shortestPathAlgorithm.Execute();
-
-                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node3).ToList();
-                shortestNodePath.Should().ContainInOrder(node1.Key, node3.Key);
-            }
-        }
 
         [TestMethod]
         public void Simple_shortest_path_with_negative_edge_weight_should_throw()
@@ -71,9 +29,9 @@ namespace Voron.Graph.Tests
                 node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
                 node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
 
-                node1.ConnectWith(tx, node2, graph,-1);
-                node1.ConnectWith(tx, node3, graph,-1);
-                node2.ConnectWith(tx, node3, graph,-1);
+                node1.ConnectWith(tx, node2, graph, -1);
+                node1.ConnectWith(tx, node3, graph, -1);
+                node2.ConnectWith(tx, node3, graph, -1);
 
                 tx.Commit();
             }
@@ -87,115 +45,9 @@ namespace Voron.Graph.Tests
             }
         }
 
-        /*
-               *   node1 ----> node3
-               *     |          /|\
-               *     L-> node2 --|
-               * 
-               */
-        [TestMethod]
-        public void Simple_shortest_path_with_varying_edge_weight_should_work()
+        protected override IShortestPathAlgorithm GetAlgorithm(Transaction tx, GraphStorage graph, Node rootNode)
         {
-            var graph = new GraphStorage("TestGraph", Env);
-
-            Node node1, node2, node3;
-            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
-            {
-                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
-                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
-                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
-
-                node1.ConnectWith(tx, node2, graph,2);
-                node1.ConnectWith(tx, node3, graph,10);
-                node2.ConnectWith(tx, node3, graph,2);
-
-                tx.Commit();
-            }
-
-            using (var tx = graph.NewTransaction(TransactionFlags.Read))
-            {
-                var shortestPathAlgorithm = new DijkstraShortestPath(tx, graph, node1, cancelTokenSource.Token);
-                var shortestPathsData = shortestPathAlgorithm.Execute();
-
-                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node3).ToList();
-                shortestNodePath.Should().ContainInOrder(node1.Key, node2.Key, node3.Key);
-            }
-        }
-
-
-        /*
-         *       ^ node2 
-         *      /        \
-         * node1          > node4
-         *      \> node3 /
-         * 
-         */
-        [TestMethod]
-        public void Between_two_equivalent_paths_first_created_should_be_chosen()
-        {
-            var graph = new GraphStorage("TestGraph", Env);
-
-            Node node1, node2, node3, node4;
-            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
-            {
-                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
-                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
-                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
-                node4 = graph.Commands.CreateNode(tx, JsonFromValue(4));
-
-                node1.ConnectWith(tx, node2, graph, 2);
-                node2.ConnectWith(tx, node4, graph, 1);
-                
-                node1.ConnectWith(tx, node3, graph, 1);
-                node3.ConnectWith(tx, node4, graph, 2);
-
-                tx.Commit();
-            }
-
-            using (var tx = graph.NewTransaction(TransactionFlags.Read))
-            {
-                var shortestPathAlgorithm = new DijkstraShortestPath(tx, graph, node1, cancelTokenSource.Token);
-                var shortestPathsData = shortestPathAlgorithm.Execute();
-
-                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node4).ToList();
-                shortestNodePath.Should().ContainInOrder(node1.Key, node2.Key, node4.Key);
-            }
-        }
-
-        /*       ^ node2 -> node3 
-         *      /                \
-         * node1 ---------------- > node4
-         */
-        [TestMethod]
-        public void Cheap_long_path_over_short_expensive_path_should_be_chosen()
-        {
-            var graph = new GraphStorage("TestGraph", Env);
-
-            Node node1, node2, node3, node4;
-            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
-            {
-                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
-                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
-                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
-                node4 = graph.Commands.CreateNode(tx, JsonFromValue(4));
-
-                node1.ConnectWith(tx, node2, graph, 1);
-                node2.ConnectWith(tx, node3, graph, 1);
-                node3.ConnectWith(tx, node4, graph, 1);
-
-                node1.ConnectWith(tx, node4, graph, 10);
-
-                tx.Commit();
-            }
-
-            using (var tx = graph.NewTransaction(TransactionFlags.Read))
-            {
-                var shortestPathAlgorithm = new DijkstraShortestPath(tx, graph, node1, cancelTokenSource.Token);
-                var shortestPathsData = shortestPathAlgorithm.Execute();
-
-                var shortestNodePath = shortestPathsData.GetShortestPathToNode(node4);
-                shortestNodePath.Should().ContainInOrder(node1.Key, node2.Key, node3.Key, node4.Key);
-            }
+            return new DijkstraShortestPath(tx, graph, rootNode, cancelTokenSource.Token);
         }
     }
 }
