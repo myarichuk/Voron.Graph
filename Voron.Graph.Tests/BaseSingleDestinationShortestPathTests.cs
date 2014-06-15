b571d6042fb2ce444f491fc03fa026a5ad2c06ab
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-06-16 01:25:45 +0300

    yet one more test

--- Voron.Graph.Tests/BaseSingleDestinationShortestPathTests.cs
+++ Voron.Graph.Tests/BaseSingleDestinationShortestPathTests.cs
@@ -81,11 +81,11 @@ namespace Voron.Graph.Tests
         }
 
         /*       ^ node2 (1,1) -> node3 (2,1)
-              *      /                     \
-              * node1(0,0) ---------------- > node4 (20,0)
-              */
+                     *      /                     \
+                     * node1(0,0) ---------------- > node4 (20,0)
+                     */
         [TestMethod]
-        public void Cheaper_but_farther_path_should_be_preferred2()
+        public void Cheaper_and_shortest_path_should_be_preferred()
         {
             var graph = new GraphStorage("TestGraph", Env);
 
@@ -102,6 +102,64 @@ namespace Voron.Graph.Tests
                 node3.ConnectWith(tx, node4, graph, 50);
 
                 node1.ConnectWith(tx, node4, graph, 1);
+
+                tx.Commit();
+            }
+
+            nodeLocations[node1.Key] = new Point
+            {
+                x = 0,
+                y = 0
+            };
+
+            nodeLocations[node2.Key] = new Point
+            {
+                x = 1,
+                y = 10
+            };
+
+            nodeLocations[node3.Key] = new Point
+            {
+                x = 2,
+                y = 10
+            };
+
+            nodeLocations[node4.Key] = new Point
+            {
+                x = 4,
+                y = 0
+            };
+
+            using (var tx = graph.NewTransaction(TransactionFlags.Read))
+            {
+                var shortestPath = GetAlgorithm(tx, graph, node1, node4).Execute();
+
+                shortestPath.Should().ContainInOrder(node1.Key, node4.Key);
+            }
+        }
+
+        /*       ^ node2 (1,1) -> node3 (2,1)
+              *      /                     \
+              * node1(0,0) ---------------- > node4 (20,0)
+              */
+        [TestMethod]
+        public void Cheaper_but_farther_path_should_be_preferred2()
+        {
+            var graph = new GraphStorage("TestGraph", Env);
+
+            Node node1, node2, node3, node4;
+            using (var tx = graph.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                node1 = graph.Commands.CreateNode(tx, JsonFromValue(1));
+                node2 = graph.Commands.CreateNode(tx, JsonFromValue(2));
+                node3 = graph.Commands.CreateNode(tx, JsonFromValue(3));
+                node4 = graph.Commands.CreateNode(tx, JsonFromValue(4));
+
+                node1.ConnectWith(tx, node2, graph, 1);
+                node2.ConnectWith(tx, node3, graph, 1);
+                node3.ConnectWith(tx, node4, graph, 1);
+
+                node1.ConnectWith(tx, node4, graph, 10);
 
                 tx.Commit();
             }
@@ -134,7 +192,7 @@ namespace Voron.Graph.Tests
             {
                 var shortestPath = GetAlgorithm(tx, graph, node1,node4).Execute();
 
-                shortestPath.Should().ContainInOrder(node1.Key, node4.Key);
+                shortestPath.Should().ContainInOrder(node1.Key, node2.Key, node3.Key, node4.Key);
             }
         }
 
