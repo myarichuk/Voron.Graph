3a5139cf6b61489a26560ee81e10870fe4bc2f2e
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-11-25 01:33:40 +0200

    outline of how Voron.Indexing will look like. Nothing is working yet.

---
+++ Voron.Indexing/BaseTermExtractor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voron.Indexing
+{
+	public abstract class BaseTermExtractor<T>
+		where T : class
+	{
+		protected readonly ITermPostProcessor[] PostProcessors;
+
+		protected BaseTermExtractor(IEnumerable<ITermPostProcessor> postProcessors)
+		{
+			PostProcessors = postProcessors.ToArray();
+		}
+
+		protected abstract IEnumerable<string> ExtractTermsFrom(T termsObject);
+
+		public IEnumerable<string> ExtractTerms(T termsObject)
+		{
+			var terms = ExtractTermsFrom(termsObject);
+			return terms.Select(term => PostProcessors.Aggregate(term, (current, postProcessor) => 
+													postProcessor.ProcessTerm(current)));
+		}
+	}
+}
---
+++ Voron.Indexing/TermPostProcessors/StopWordRemover.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voron.Indexing.TermPostProcessors
+{
+	public class StopwordRemover : ITermPostProcessor
+	{
+		private readonly string[] _stopwords = new[]
+		{
+			"a",
+			"an",
+			"and",
+			"are",
+			"as",
+			"at",
+			"be",
+			"but",
+			"by",
+			"for",
+			"if",
+			"in",
+			"into",
+			"is",
+			"it",
+			"no",
+			"not",
+			"of",
+			"on",
+			"or",
+			"such",
+			"that",
+			"the",
+			"their",
+			"then",
+			"there",
+			"these",
+			"they",
+			"this",
+			"to",
+			"was",
+			"will",
+			"with"
+		};
+
+		public string ProcessTerm(string term)
+		{
+			
+		}
+	}
+}
