fb2e997ca42e2ec0ebf0d33c48748ff1a535d173
Michael Yarichuk <michael.yarichuk@gmail.com>
2015-03-21 17:00:50 +0200

    now the test Should_persist_stored_field_names actually tests something

--- Voron.Indexing.Testing/InfrastructureTests.cs
+++ Voron.Indexing.Testing/InfrastructureTests.cs
@@ -2,32 +2,53 @@
 using Xunit;
 using FluentAssertions;
 using Voron.Indexing.Tokenizers;
+using System;
+using System.IO;
 
 namespace Voron.Indexing.Testing
 {
-	public class InfrastructureTests
+	public class InfrastructureTests : IDisposable
 	{
-		[Fact]
+		private readonly string currentFolder;
+		private readonly string dataDir;
+		public InfrastructureTests()
+		{
+			currentFolder = new FileInfo(GetType().Assembly.Location).Directory.FullName;
+			dataDir = currentFolder + Path.DirectorySeparatorChar + "Data";
+        }
+
+		[Fact(DisplayName = "Stopword filter should remove all stopwords")]
 		public void StopwordFilterShouldRemoveAllStopwords()
 		{
 			const string testString = @"This has gone too far. A FooBar is needed to restore all the honor that has been lost.Oh yeah? And who is going to stop me?! You and what army, eh?";
 			const string expectedProcessedString = " has gone too far.  FooBar  needed  restore all  honor  has been lost.Oh yeah?  who  going  stop me?! You  what army, eh?";
 			var stopwordsRemover = new StopwordValueFilter(1);
-			
+
 			var processedString = stopwordsRemover.ProcessTerm(testString);
 			processedString.Should().Be(expectedProcessedString);
 		}
 
-		[Fact]
-		public void Can_persist_stored_field_names()
+		[Fact(DisplayName = "Should persist stored field names")]
+		public void Should_persist_stored_field_names()
 		{
-            using (var env = new StorageEnvironment(StorageEnvironmentOptions.ForPath("\\")))
-            {
-                var index = new Index("TestIndex",env,new NGramTokenizer());
-                index.AddIndexedField("foo");
-                index.AddIndexedField("bar");
-                index.AddIndexedField("baa");
-            }
+			using (var env = new StorageEnvironment(StorageEnvironmentOptions.ForPath(dataDir)))
+			{
+				var index = new Index("TestIndex", env, new NGramTokenizer());
+				index.AddIndexedField("foo");
+				index.AddIndexedField("bar");
+				index.AddIndexedField("baa");
+			}
+
+			using (var env = new StorageEnvironment(StorageEnvironmentOptions.ForPath(dataDir)))
+			{
+				var index = new Index("TestIndex", env, new NGramTokenizer());
+				index.IndexedFields.Should().Contain("foo", "bar", "baa");
+			}
 		}
+
+		public void Dispose()
+		{
+			Directory.Delete(dataDir, true);
+        }
 	}
 }
